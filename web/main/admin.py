3e5c7f26b11ee69649ef2649a4cad09158250931
Muminjanov Akbar <muminjanov.75@gmail.com>
2024-03-09 11:16:56 +0200

    fixed mistakes

--- web/main/admin.py
+++ web/main/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register([User, Category, SubCategory, Product, History])
+admin.site.register([User, History])
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+
+@admin.register(SubCategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['product_id', 'name', 'price', 'available']
+    search_fields = ['product_id', 'name', 'price']
+
+
